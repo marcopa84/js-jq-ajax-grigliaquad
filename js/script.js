00ca3b0e3979a704bbfc23c01a2e2b7d0102f6a7
Marco <marcopa84@hotmail.it>
2020-01-30 14:43:37 +0100

    ajax OK css OK

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-alert();
+$(document).ready(function () {
+  var num = 2;
+
+  $('.square').click(function () {
+    $.ajax(
+      {
+        url: "https://flynn.boolean.careers/exercises/api/random/int",
+        method: "GET",
+        success: function (data, stato) {
+          console.log(data.response);
+        },
+        error: function (richiesta, stato, errori) {
+          alert("E' avvenuto un errore. " + errore);
+        }
+      }
+    );
+    $(this).text(num);
+    if (num <= 5) {
+      $(this).addClass('yellow');
+    }else {
+      $(this).addClass('red');
+    }
+  });
+
+});
